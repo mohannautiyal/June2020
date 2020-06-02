6c2d03e0972161f86ba779895e607d88a7efde95
mohannautiyal <mohannautiyal80@gmail.com>
2020-06-02 22:54:52 +0530

    API ResponseBody

---
+++ API_cont/src/main/java/com/home/SampleAPI/APIresponseBody.java
@@ -0,0 +1,31 @@
+package com.home.SampleAPI;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+public class APIresponseBody {
+
+	public static void main(String[] args) {
+
+		
+		RestAssured.baseURI ="http://restapi.demoqa.com/utilities/weather/city";
+		RequestSpecification rs = RestAssured.given();
+		Response rsp = rs.get("/Gurgaon");
+		ResponseBody rsb = rsp.getBody();
+		
+		
+		System.out.println(rsb.asString());
+		
+		// Using json path
+		//Response interface gives you a mechanism to extract nodes based on a given JsonPath. There is a method called Response.JsonPath(), which returns a io.restassured.path.json.JsonPath Object. This object can be used to further query specific parts of the Response Json.
+		
+		String city = rsb.jsonPath().get("City");
+		System.out.println("City is " +city);
+		
+		
+		
+	}
+
+}
