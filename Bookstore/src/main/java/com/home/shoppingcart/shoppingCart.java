e74abce99789a50472ee6e14577f9756aec294a2
mohannautiyal <mohannautiyal80@gmail.com>
2020-06-30 22:21:39 +0530

    sum cost of Books using Stream API

--- Bookstore/src/main/java/com/home/shoppingcart/shoppingCart.java
+++ Bookstore/src/main/java/com/home/shoppingcart/shoppingCart.java
@@ -6,25 +6,24 @@ import java.util.List;
 import com.home.entity.book;
 
 public class shoppingCart {
-	
+
 	List<book> itemsInCart = new ArrayList<book>();
-	
+
 	public void addBook(book book) {
 		itemsInCart.add(book);
-	  }
-	
+	}
+
 	public void checkout() {
-		if (itemsInCart.size()==0)
+		if (itemsInCart.size() == 0)
 			System.out.println("You can't checkout an empty cart !");
-		else
-		{
-		      int totalcost=0;
-			/*
-			 * itemsInCart.stream().forEach(x -> (x.getCost()+totalcost));
-			 * System.out.println("Total cost is "+totalcost);
-			 */
-			
-		};
+		else {
+
+			int totalcost = itemsInCart.stream().mapToInt(x -> x.getCost()).sum();
+
+			System.out.println("Total cost is " + totalcost);
+
+		}
+		;
 	}
 
 }
